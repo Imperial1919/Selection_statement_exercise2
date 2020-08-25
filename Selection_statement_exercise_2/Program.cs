1f8a5671999a2e14b5d814cb904f4f8cb09f8a72
Bryce <ImperialEagle1998@gmail.com>
2020-08-25 13:11:16 -0500

    Add project files.

---
+++ Selection_statement_exercise_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Selection_statement_exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello hello hello there youngster!, what is Your favorite subject in public day camp-.. public school?");
+
+            string reply = Console.ReadLine();
+            
+            switch (reply)
+            {
+                case "History":
+                    Console.WriteLine("Oh yes, that is my favorite subject as well. Makes you realize how far our system has decayed-.. advanced since times long... loooong past us.");
+                    break;
+
+                case "Math":
+                    Console.WriteLine("Ah yes, Math, a headache embodied in a subject for those who rely on memorization or easy to read examples like myself.");
+                    break;
+
+                case "Science":
+                    Console.WriteLine(".. Ok, I can get behind that, science and physics are quite interesting after all Kiddo. Can lead to a probable sucessful career too.");
+                    break;
+
+                case "Art":
+                    Console.WriteLine("A admirable favorite subject, I was in art 4 in school, Just don't try to turn it into a career because reality will slap You in the face.. \n With a razor blade.");
+                    break;
+                case "English":
+                    Console.WriteLine(".. Ew.");
+                    break;
+                default:
+                    Console.WriteLine(".. What?");
+                    break;
+            }
+
+                
+
+        }
+    }
+}
